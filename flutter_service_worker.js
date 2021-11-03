d945285cf92bebe05d345f8498f369836dfed058
arunkarthik <arun@geodexia.com>
2021-11-03 23:47:39 +0530

    Video Player

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "405967a97221e12894b52923d502d1bd",
-"index.html": "9b27cf935ac8672812e84b8b4c3cc855",
-"/": "9b27cf935ac8672812e84b8b4c3cc855",
-"main.dart.js": "53e20e158c611c9ccd71b849e582aabc",
+"index.html": "4750241771296cd3a2d30334293ce1e7",
+"/": "4750241771296cd3a2d30334293ce1e7",
+"main.dart.js": "bb113d52dbcecf3e978880fc66b2a8ae",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
